a1a883eef7ed45d56048154cd8773080d89803f9
Sang Nguyen <sang@paradox.ai>
2019-07-25 15:01:56 +0700

    fix env

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(os.environ("GOOGLE_APPLICATION_CREDENTIALS"), scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("GAPO").sheet1
 
-sheet.update_cell(1, 1, "I just wrote to a spreadsheet using Python!")
+sheet.update_cell(1, 1, 'I just wrote to a spreadsheet using Python!')
