d8aa4df9e52b4f0c41435a42b08d0e178d557ab4
Sang Nguyen <sang@paradox.ai>
2019-07-25 16:20:41 +0700

    fix datetime

--- user.py
+++ user.py
@@ -37,4 +37,4 @@ class User():
             else:
                 self.data[key] = kwargs[key]
     def get_value(self):
-        return [ i for i in self.data.values() ]
+        return [ str(i) for i in self.data.values() ]
