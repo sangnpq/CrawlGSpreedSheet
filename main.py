7050831d22fa338b3401f6b367f700c8223f98e6
Sang Nguyen <sang@paradox.ai>
2019-07-25 16:15:49 +0700

    remove threading

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
 import gspread
 import os
 from oauth2client.service_account import ServiceAccountCredentials
-import concurrent.futures
 import requests
-import threading
-import time
 import json
 from user import User
 
-thread_local = threading.local()
-
-
-def get_session():
-    if not hasattr(thread_local, "session"):
-        thread_local.session = requests.Session()
-    return thread_local.session
-
 
 def download_site(url):
-    session = get_session()
-    with session.get(url) as response:
+    with requests.get(url) as response:
         item = json.loads(response.text)[0]
         user = User(**item)
         writeSpreedSheet(user, user.data['id'])
-
-
-def download_all_sites(sites):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.map(download_site, sites)
-
 
 def writeSpreedSheet(user, index):
     '''
@@ -47,7 +29,5 @@ if __name__ == "__main__":
     url = "https://api.gapo.vn/main/v1.0/user?id=%d"
     limit = 200000
     sites = [ url %(i + 1) for i in range(0, limit) ]
-
-    start_time = time.time()
-    download_all_sites(sites)
-    duration = time.time() - start_time
+    for site in sites:
+        download_site(site)
