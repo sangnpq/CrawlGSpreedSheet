3ce026959569352a9520306b21e84a6bcafd9fd9
Sang Nguyen <sang@paradox.ai>
2019-07-25 14:54:35 +0700

    new main

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+scope = ['https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(env("GOOGLE_APPLICATION_CREDENTIALS"), scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("GAPO").sheet1
