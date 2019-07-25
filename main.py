00dcc8de478f410acab19e2e2cb3c018e14d4bcb
Sang Nguyen <sang@paradox.ai>
2019-07-25 17:25:21 +0700

    fix gspread limit request

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def writeSpreedSheet(data, index):
     '''
         Use creds to create a client to interact with the Google Drive API
     '''
-    sheet.values_update('Sheet1!A%d' %index, params={'valueInputOption': 'RAW'}, body={ 'values': data }
+    sheet.values_update('Sheet1!A%d' %index, params={'valueInputOption': 'RAW'}, body={ 'values': data })
 
 
 if __name__ == "__main__":
